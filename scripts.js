9647dcc2ae03bc1a0bd564eb30fce6c72be869d3
NitaLewska <Nita@ya.ru>
2021-07-20 19:59:15 +0300

    JS

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const newUser = {
     email: undefined,
 };
 
-document.getElementById("submitForm").addEventListener('click', validation);
+document.getElementById("submit_form").addEventListener('click', validation);
 
 function validation(e) {
     e.preventDefault();
@@ -12,11 +12,14 @@ function validation(e) {
     const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (emailReg.test(newUser.email) === false) {
-        document.getElementById("emailError").classList.remove('hidden');
+        document.getElementById("error_text").classList.remove('hidden');
+        document.getElementById("error_icon").classList.remove('hidden');        
         document.getElementById("email").classList.add('error')
     } else {
         document.getElementById("emailError").classList.add('hidden');
+        document.getElementById("error_icon").classList.add('hidden');    
         document.getElementById("email").classList.remove('error')
+        document.getElementById("email").value = "";
     }
    
 }
